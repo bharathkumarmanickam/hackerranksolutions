f439b6a244e34734e2fa7f142d76db688fd03e80
Bharath Kumar Manickam <69282130+bharathkumarmanickam@users.noreply.github.com>
2021-09-13 21:54:21 +0530

    new codings

--- java_problems/strings/stringintro.java
+++ java_problems/strings/stringintro.java
@@ -1,5 +1,35 @@
 package strings;
 
+import java.io.*;
+import java.util.*;
+
 public class stringintro {
-    
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        System.out.println(A.length()+B.length());
+        
+        
+        // For Lexicographical
+        int result = A.compareTo(B);
+        if(result <= 0)System.out.println("No");
+        else System.out.println("Yes");
+        
+        
+        // For A
+        String a = A.substring(0,1);
+        String rema = A.substring(1);
+         //For B
+        String b = B.substring(0,1);
+        String remb = B.substring(1);     
+        System.out.println(a.toUpperCase()+rema+" "+b.toUpperCase()+remb);
+        
+       
+    }
 }
+
+
+
---
+++ java_problems/strings/substring.java
@@ -0,0 +1,21 @@
+package strings;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class substring {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        
+        //have to print the substring in the give string
+        
+        System.out.println(S.substring(start,end));
+    }
+}
